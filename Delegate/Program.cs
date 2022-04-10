548a33178084599ccb2bd4641556814ac5e023c2
burcutas636@gmail.com <burcutas636@gmail.com>
2022-04-10 14:12:03 +0300

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Delegate;
+
+CustomerManager customerManager = new CustomerManager();
+MyDelegate myDelegate = customerManager.SendMessage;
+myDelegate += customerManager.ShowAlert;
+myDelegate -= customerManager.SendMessage;
+
+MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+myDelegate2 += customerManager.ShowAlert2;
+
+Matematik matematik = new Matematik();
+MyDelegate3 myDelegate3 = matematik.Topla;
+myDelegate3 += matematik.Carp;
+
+Console.WriteLine(myDelegate3(2, 4)); 
+myDelegate2("Hello...");
+myDelegate();
+
+public delegate void MyDelegate();
+public delegate void MyDelegate2(string text);
+public delegate int MyDelegate3(int number1, int number2);
+
+
+
+
+
+
+
+
